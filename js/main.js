7d06ff0f16ad8532bb220a71dbab172453b65dc5
Davide <Sbalzerdavide@hotmail.it>
2020-11-26 11:59:18 +0100

    clean code milestone 1, 2

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const app = new Vue ({
         apiKey: 'e6b299c9bc3d24b73c05ea01c7f18871',
         search: '',
         list: [],
+        flagList: ['it', 'en'],
 
     },
     created(){
@@ -13,51 +14,56 @@ const app = new Vue ({
         makeResearch(){
             if (this.search != 0 ){
                 this.list = [];
-                this.movie();
-                this.tv();
-                this.search= '';
-            };
-        },
-        movie(){
-            axios.get('https://api.themoviedb.org/3/search/movie', {
-                params: {
-                    api_key: this.apiKey,
-                    query: this.search,
-                    language: 'it-IT',
-                }
-            })
-            .then(response => {
-                console.log(response.data.results);
-                this.list = response.data.results;
-            })
-            .catch(error =>{
-                console.log('eror: ', error);
-            })
-        },
-        tv(){
-            axios.get('https://api.themoviedb.org/3/search/tv', {
-                params: {
-                    api_key: this.apiKey,
-                    query: this.search,
-                    language: 'it-IT',
-                }
-            })
-            .then(response => {
-                for (let i = 0; i < response.data.results.length; i++){
-                    this.list.push({
-                        vote_average: response.data.results[i].vote_average,
-                        title: response.data.results[i].name,
-                        original_title: response.data.results[i].original_name,
-                        original_language: response.data.results[i].original_language,
+                // api call for movie 
+                axios.get('https://api.themoviedb.org/3/search/movie', {
+                    params: {
+                        api_key: this.apiKey,
+                        query: this.search,
+                        language: 'it-IT',
+                    }
+                })
+                .then(response => {
+                    console.log(response.data.results);
+                    this.list = response.data.results;
+
+                    // api call for tv series 
+                    axios.get('https://api.themoviedb.org/3/search/tv', {
+                        params: {
+                            api_key: this.apiKey,
+                            query: this.search,
+                            language: 'it-IT',
+                        }
                     })
+                    .then(response => {
+                        // push tv object with the same key of movie object
+                        for (let i = 0; i < response.data.results.length; i++){
+                            this.list.push({
+                                vote_average: response.data.results[i].vote_average,
+                                title: response.data.results[i].name,
+                                original_title: response.data.results[i].original_name,
+                                original_language: response.data.results[i].original_language,
+                            })
+                        };
+                    })
+                    .catch(error =>{
+                        console.log('eror: ', error);
+                    });
+
+
+                    // clear input text
+                    this.search= '';
+                })
+                .catch(error =>{
+                    console.log('eror: ', error);
+                })
                 };
-            })
-            .catch(error =>{
-                console.log('eror: ', error);
-            });
         },
-        takeVote(ind){
-            return Math.ceil(this.list[ind].vote_average /2 );
+        takeVote(vote){
+            return Math.ceil(vote /2 );
         },
+        flag(lang){
+            return this.flagList.includes(lang);
+
+        }
     }
 });
